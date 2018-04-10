ab12fbeae17e27e412d639d0da026e41cb92fbb0
findlaysmith <ferelach45@gmail.com>
2018-04-10 23:53:43 +0100

    changing language detection to use offline module, for IO load/privacy

--- ircbot/plugins/translate.py
+++ ircbot/plugins/translate.py
@@ -1,12 +1,13 @@
 import requests
 import json
 
+from langdetect import detect, detect_langs
 from ircbot import bot
 
 
 def translate(bot, text):
     api_key = bot.config['Yandex']['translate_key']
-    lang = check_lang(bot, text)
+    lang = detect(text)
     translate_url = "https://translate.yandex.net/api/v1.5/tr.json/translate?key={}&text={}&lang={}-{}".format(api_key, text, lang, bot.config["System"]["lang"])
     api_response = requests.get(translate_url).text
     api_json = json.loads(api_response)
@@ -14,15 +15,8 @@ def translate(bot, text):
     return api_json['text'][0]
 
 
-def check_lang(bot, text):
-    api_key = bot.config['Yandex']['translate_key']
-    detect_url = "https://translate.yandex.net/api/v1.5/tr.json/detect?key={}&text={}".format(api_key, text)
-    detect_response = requests.get(detect_url).text
-    detect_json = json.loads(detect_response)
-    return detect_json["text"]
-
-
 @bot.hook()
 def message_hook(bot, channel, sender, message):
-    if check_lang(bot, message) != bot.config["System"]["lang"]:
+    # gibberish messages are still assigned a language, however the level of confidence is always less than 0.9
+    if detect(message) != bot.config["System"]["lang"] and float(str(detect_langs(message)[0])[3:]) < 0.9:
         bot.message(channel, "translation: {}").format(translate(bot, message))
